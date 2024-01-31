a5988809be95d04b0c4ed81564e56a181bcbb632
mz-imhj <imhj@mz.co.kr>
2024-01-31 10:47:24 +0900

    remove unnecessary component modify menu list

--- src/views/apps/email/index.js
+++
@@ -1,6 +0,0 @@
-import Compose from './compose/Compose';
-import Inbox from './inbox/Inbox';
-import Message from './message/Message';
-import Template from './Template';
-
-export { Compose, Inbox, Message, Template };
