b010788a5ae88de01ec5b48fc7bc678621fcf28f
mz-imhj <143996205+mz-imhj@users.noreply.github.com>
2024-02-01 15:09:43 +0900

    Feature/add sign in page (#1)
    * Turn off prop validation detection
    
    * Add SignIn page
    Add SignIn Service
    Add Custom Toast Component
    
    * Add .env to gitignore
    
    * Remove forgot password link
    
    * Add validation
    
    * Change SignOut Icon

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
+import { RecoilRoot } from 'recoil';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <RecoilRoot>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </RecoilRoot>
 );
 
 // If you want to start measuring performance in your app, pass a function
