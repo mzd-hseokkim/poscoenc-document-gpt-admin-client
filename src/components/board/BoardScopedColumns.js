26e56e67b6f5bf9d48063a1beef73a1a8dbb549e
SonYongGyun <135930140+mzd-omega@users.noreply.github.com>
2024-02-02 09:25:15 +0900

    modify table layout (#4)

--- src/components/board/BoardScopedColumns.js
+++ src/components/board/BoardScopedColumns.js
@@ -3,19 +3,17 @@ import { CBadge } from '@coreui/react-pro';
 const translate = (deleted) => {
   return deleted === 'false' ? 'OnBoard' : 'Deleted';
 };
-export const getScopedColumns = (getBadge, toggleContent, content) => ({
-  title: (item) => (
-    <td
-      onClick={() => {
-        toggleContent(item.id);
-      }}
-    >
-      {item.title}
-    </td>
-  ),
-  deleted: (item) => (
+export const getScopedColumns = (getBadge) => ({
+  ID: (item) => <td>{item.id}</td>,
+  제목: (item) => <td>{item.title}</td>,
+  작성자: (item) => <td>{item.createdByName}</td>,
+  상태: (item) => (
     <td>
       <CBadge color={getBadge(item.deleted)}>{translate(item.deleted)}</CBadge>
     </td>
   ),
+  작성일: (item) => <td>{item.createdAt.substring(0, 10)}</td>,
+  //REMIND fix icon for attachment
+  첨부파일: (item) => <td>{item.hasAttachement ? 'icon' : 'X'}</td>,
+  조회수: (item) => <td>{item.viewCount}</td>,
 });
