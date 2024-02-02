a71993d277d20993f762772dbd634739dc5214e5
mz-imhj <143996205+mz-imhj@users.noreply.github.com>
2024-02-02 17:50:05 +0900

    Fix ToastContainer (#5)

--- src/components/toast/SimpleToastContainer.js
+++ src/components/toast/SimpleToastContainer.js
@@ -1,21 +1,23 @@
-import { CToast, CToastBody, CToastClose, CToaster } from '@coreui/react-pro';
+import React from 'react';
 import { useRecoilValue } from 'recoil';
+import { CToast, CToastBody, CToastClose, CToaster } from '@coreui/react-pro';
 import { toastState } from '../../states/toastState';
 
 const SimpleToastContainer = () => {
   const toasts = useRecoilValue(toastState);
 
   return (
-    <CToaster placement="bottom-end">
-      {toasts?.map((toast) => (
-        <CToast key={toast.id} autohide={true} visible={true} color={toast.color}>
-          <div className="d-flex">
-            <CToastBody className="text-white">{toast.body}</CToastBody>
+    <CToaster
+      placement="bottom-end"
+      push={
+        toasts ? (
+          <CToast autohide visible color={toasts.color} className="d-flex">
+            <CToastBody className="text-white">{toasts.message}</CToastBody>
             <CToastClose className="me-2 m-auto" white />
-          </div>
-        </CToast>
-      ))}
-    </CToaster>
+          </CToast>
+        ) : undefined
+      }
+    />
   );
 };
 
