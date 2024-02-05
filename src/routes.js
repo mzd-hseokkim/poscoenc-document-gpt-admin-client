3f6c0632e1b4b06ea72d4eea29af74be11aa0823
SonYongGyun <omega@mz.co.kr>
2024-02-05 17:09:58 +0900

    modify routes to Korean

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ const UserManagement = React.lazy(() => import('./views/pages/user/UserManagemen
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/icons', exact: true, name: 'Icons', element: CoreUIIcons },
-  { path: '/boards', exact: true, name: 'Board', element: BoardMain },
+  { path: '/boards', exact: true, name: '게시판', element: BoardMain },
   { path: '/user-accounts/management', exact: true, name: '사용자 관리', element: UserManagement },
 ];
 
