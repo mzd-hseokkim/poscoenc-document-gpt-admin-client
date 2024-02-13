28fa7667658dc8bcd4d4003034d618625ef0a859
SonYongGyun <omega@mz.co.kr>
2024-02-13 15:50:38 +0900

    Add Comment form label

--- src/components/board/BoardComments.js
+++ src/components/board/BoardComments.js
@@ -1,12 +1,13 @@
-import { CFormInput, CFormTextarea, CInputGroup, CInputGroupText } from '@coreui/react-pro';
+import { CFormInput, CFormLabel, CFormTextarea, CInputGroup, CInputGroupText } from '@coreui/react-pro';
 import { format } from 'date-fns';
 
-const BoardComments = ({ formData, handleSubmitComment }) => {
+const BoardComments = ({ formData }) => {
   return (
     <div className="comments-section">
+      <CFormLabel htmlFor="postComments">댓글</CFormLabel>
       {formData?.comments?.map((comment, index) => (
         <div key={index} className="comment-item mb-2">
-          <CInputGroup className="mb-1">
+          <CInputGroup id="postComments" className="mb-1">
             <CFormInput value={comment.createdByName} readOnly />
             <CInputGroupText>{format(new Date(comment.createdAt), 'yyyy/MM/dd HH:mm:ss')}</CInputGroupText>
             {/*REMIND 신고 버튼, 답글 작성 기능추가 */}
@@ -16,11 +17,10 @@ const BoardComments = ({ formData, handleSubmitComment }) => {
           </CInputGroup>
         </div>
       ))}
-
       <CInputGroup className="mb-3">
         <CInputGroupText>댓글</CInputGroupText>
         <CFormTextarea placeholder="댓글을 입력해주세요..." />
-        <CInputGroupText onClick={handleSubmitComment} style={{ cursor: 'pointer' }}>
+        <CInputGroupText onClick={null} style={{ cursor: 'pointer' }}>
           작성
         </CInputGroupText>
       </CInputGroup>
