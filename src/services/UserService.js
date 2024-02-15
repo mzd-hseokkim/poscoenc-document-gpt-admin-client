b92056734c89cc2c29487b33c8009d50592624c8
mz-imhj <143996205+mz-imhj@users.noreply.github.com>
2024-02-15 17:44:49 +0900

    Add User account Table (#16)
    Add UserDetailForm
    Add User Delete Restore Feature

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,6 @@
 import api from '../api/Api';
 
-const createUser = async (payload) => {
+const postUser = async (payload) => {
   const response = await api.post('/admin/user-accounts', payload);
   return response.data;
 };
@@ -11,9 +11,6 @@ const getUserList = async (params) => {
       email: params.email,
       team: params.team,
       memo: params.memo,
-      fromLastPullRequest: params.fromLastPullRequest,
-      toLastPullRequest: params.toLastPullRequest,
-      findEmptyPullRequest: params.findEmptyPullRequest,
       deletionOption: params.deletionOption,
       page: params.page,
       size: params.size,
@@ -27,26 +24,28 @@ const getUserDetail = async (id) => {
   return response.data;
 };
 
-const updateUser = async (id, payload) => {
+const putUserDetail = async (id, payload) => {
   const response = await api.put(`/admin/user-accounts/${id}`, payload);
   return response.data;
 };
 
-const deleteSingleUser = async (id, deleted) => {
+const patchDeleteSingleUser = async (id, deleted) => {
   const response = await api.patch(`/admin/user-accounts/${id}/deleted?deleted=${deleted}`);
   return response.data;
 };
 
-const deleteMultipleUser = async (ids, deleted) => {
+const patchDeleteMultipleUser = async (ids, deleted) => {
   const response = await api.patch(`/admin/user-accounts/deleted/${deleted}`, ids);
   return response.data;
 };
 
-export default {
+const UserService = {
   getUserList,
   getUserDetail,
-  createUser,
-  updateUser,
-  deleteSingleUser,
-  deleteMultipleUser,
+  postUser,
+  putUserDetail,
+  patchDeleteSingleUser,
+  patchDeleteMultipleUser,
 };
+
+export default UserService;
