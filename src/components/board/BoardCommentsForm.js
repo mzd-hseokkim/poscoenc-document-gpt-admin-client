21feb239c02b3ca6ef25b656735d2582d6e9c548
SonYongGyun <omega@mz.co.kr>
2024-02-15 10:57:49 +0900

    add post comment create, delete service

--- src/components/board/BoardCommentsForm.js
+++ src/components/board/BoardCommentsForm.js
@@ -1,15 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 
-import {
-  CButton,
-  CForm,
-  CFormInput,
-  CFormLabel,
-  CFormText,
-  CFormTextarea,
-  CInputGroup,
-  CInputGroupText,
-} from '@coreui/react-pro';
+import { CButton, CForm, CFormLabel, CFormText, CFormTextarea, CInputGroup, CInputGroupText } from '@coreui/react-pro';
 import { format } from 'date-fns';
 import { useRecoilValue } from 'recoil';
 
@@ -22,12 +13,14 @@ const BoardCommentsForm = ({ postId }) => {
   const [commentText, setCommentText] = useState('');
   const boardPostComments = useBoardPostComments(postId);
   const submitComment = useSubmitComment();
-  const { deleteComment } = useDeleteComment();
+  //REMIND add loading state
+  const { deleteComment, deleteIsLoading } = useDeleteComment();
   const currentUserId = useRecoilValue(userIdSelector);
+
+  // create, delete  -------------------------------------------------------------
   const handleCommentChange = (event) => {
     setCommentText(event.target.value);
   };
-
   const handleCommentSubmit = async (e) => {
     e.preventDefault();
     await submitComment.submitComment(postId, commentText);
@@ -36,16 +29,12 @@ const BoardCommentsForm = ({ postId }) => {
   };
 
   const toggleCommentStatus = async (commentId, shouldDelete) => {
-    try {
-      const isSuccess = await deleteComment(commentId, shouldDelete);
-      if (isSuccess) {
-        await boardPostComments.fetchPostComments();
-      }
-    } catch (e) {
-      console.log(e);
-    } finally {
+    const isSuccess = await deleteComment(commentId, shouldDelete);
+    if (isSuccess) {
+      await boardPostComments.fetchPostComments();
     }
   };
+  // create, delete --------------------------------------------------------------
 
   // scroll ------------------------------------------------------------
   const endOfCommentsRef = useRef(null);
@@ -58,26 +47,15 @@ const BoardCommentsForm = ({ postId }) => {
   return (
     <CForm onSubmit={handleCommentSubmit} className="comments-section">
       <CFormLabel htmlFor="postComments">댓글</CFormLabel>
-      <div style={{ height: '180px', overflowY: 'auto' }}>
+
+      <div style={{ height: '180px', overflowY: 'auto' }} ref={endOfCommentsRef}>
         {boardPostComments.postComments?.map((comment, index) => (
           <div key={index} className={`comment-item mb-2 ${comment.deleted ? 'deleted' : ''}`}>
             <CInputGroup className="mb-1">
               <CFormText readOnly>{`작성자 : ${comment.createdByName}`}</CFormText>
-              {/*REMIND 댓글 작성자 id 를 받아오게 수정 요청*/}
               {comment.createdByName === currentUserId && (
                 <div className="ms-auto">
-                  <CButton
-                    size="sm"
-                    className="me-2"
-                    //StartFrom onClick={() => handleEdit(comment)}
-                  >
-                    수정
-                  </CButton>
-                  <CButton
-                    size="sm"
-                    className="text-danger"
-                    onClick={() => toggleCommentStatus(comment.id, !comment.deleted)}
-                  >
+                  <CButton size="sm" onClick={() => toggleCommentStatus(comment.id, !comment.deleted)}>
                     {comment.deleted ? '복구' : '삭제'}
                   </CButton>
                 </div>
@@ -86,31 +64,21 @@ const BoardCommentsForm = ({ postId }) => {
                 {`작성일시 : ${format(new Date(comment.createdAt), 'yyyy/MM/dd HH:mm:ss')}`}
               </CFormText>
             </CInputGroup>
-            {comment.createdByName === currentUserId ? (
-              <CFormInput
-                className="mb-1 p-2"
-                disabled={comment.deleted}
-                defaultValue={comment.content}
-                // readOnly={!isEditing || editingCommentId !== comment.id}
-              />
-            ) : (
-              <CFormText
-                className="mb-1 p-2"
-                style={{
-                  textDecoration: comment.deleted ? 'line-through' : 'none',
-                  height: '30px',
-                  fontSize: 'small-caption',
-                }}
-                readOnly
-              >
-                {comment.content}
-              </CFormText>
-            )}
+            <CFormText
+              className="mb-1 p-2"
+              style={{
+                textDecoration: comment.deleted ? 'line-through' : 'none',
+                height: '30px',
+                fontSize: 'small-caption',
+              }}
+              readOnly
+            >
+              {comment.content}
+            </CFormText>
           </div>
         ))}
-
-        <div ref={endOfCommentsRef} />
       </div>
+
       <CInputGroup className="mb-3 mt-4">
         <CInputGroupText>댓글</CInputGroupText>
         <CFormTextarea placeholder="댓글을 입력해주세요." value={commentText} onChange={handleCommentChange} />
