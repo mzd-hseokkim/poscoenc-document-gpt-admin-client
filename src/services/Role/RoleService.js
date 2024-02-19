25c14f949150dbc6a9d43ec772de13b1b23adf1e
mz-imhj <imhj@mz.co.kr>
2024-02-19 17:30:16 +0900

    Add Role Management Page

--- src/services/Role/RoleService.js
+++ src/services/Role/RoleService.js
@@ -1,16 +1,16 @@
 import api from '../../api/Api';
 
-const getRoleList = async () => {
+const getRoles = async () => {
   const response = await api.get('/admin/roles');
   return response.data;
 };
 
-const getSingleRole = async (id) => {
+const getRole = async (id) => {
   const response = await api.get(`/admin/roles/${id}`);
   return response.data;
 };
 
-const createRole = async (roleName) => {
+const postRole = async (roleName) => {
   const response = await api.post('/admin/roles', roleName, {
     headers: {
       'Content-Type': 'text/plain',
@@ -19,7 +19,7 @@ const createRole = async (roleName) => {
   return response.data;
 };
 
-const updateRole = async ({ id, roleName }) => {
+const putRole = async (id, roleName) => {
   const response = await api.put(`/admin/roles/${id}`, roleName, {
     headers: {
       'Content-Type': 'text/plain',
@@ -28,21 +28,23 @@ const updateRole = async ({ id, roleName }) => {
   return response.data;
 };
 
-const deleteSingleRole = async (id, deleted) => {
+const deleteRole = async (id, deleted) => {
   const response = await api.patch(`/admin/roles/${id}/deleted?deleted=${deleted}`);
   return response.data;
 };
 
-const deleteMultipleRole = async (ids, deleted) => {
+const deleteRoles = async (ids, deleted) => {
   const response = await api.patch(`/admin/roles/deleted/${deleted}`, ids);
   return response.data;
 };
 
-export default {
-  getRoleList,
-  getSingleRole,
-  createRole,
-  updateRole,
-  deleteSingleRole,
-  deleteMultipleRole,
+const RoleService = {
+  getRoles,
+  getRole,
+  postRole,
+  putRole,
+  deleteRole,
+  deleteRoles,
 };
+
+export default RoleService;
---
+++ src/utils/role/roleColumnConfig.js
@@ -0,0 +1,37 @@
+export const roleColumnConfig = [
+  {
+    key: 'id',
+    label: '아이디',
+    _style: { width: '9%' },
+  },
+  {
+    key: 'role',
+    label: '권한',
+    _style: { width: '18%' },
+  },
+  {
+    key: 'createdByName',
+    label: '생성자',
+    _style: { width: '12%' },
+  },
+  {
+    key: 'createdAt',
+    label: '생성일',
+    _style: { width: '17%' },
+  },
+  {
+    key: 'modifiedByName',
+    label: '수정자',
+    _style: { width: '15%' },
+  },
+  {
+    key: 'modifiedAt',
+    label: '수정일',
+    _style: { width: '17%' },
+  },
+  {
+    key: 'deleted',
+    label: '삭제',
+    _style: { width: '12%' },
+  },
+];
