14ac40751dfe9904cae19132d9d39b17f8ad1f54
mz-imhj <imhj@mz.co.kr>
2024-02-22 16:05:19 +0900

    Add Validation with react-hook-form Add Validation to InputList Modify MenuDetailForm error handling

--- src/components/input/InputList.js
+++ src/components/input/InputList.js
@@ -1,4 +1,5 @@
 import { CCol, CFormInput, CRow } from '@coreui/react-pro';
+import { Controller } from 'react-hook-form';
 
 import { formatToYMD } from '../../utils/common/dateUtils';
 
@@ -14,26 +15,39 @@ const getFieldValue = (field, formData) => {
   return formData[field.name];
 };
 
-const InputList = ({ fields, formData, handleChange, isReadMode }) => {
-  return fields?.map((field) => {
+const InputList = ({ fields, control, isReadMode, errors, formData }) => {
+  return fields.map((field) => {
     const isRendered = field.isRendered ?? true;
-    const fieldValue = getFieldValue(field, formData);
-
     if (isRendered) {
       return (
         <CRow className="mb-3" key={field.name}>
           <CCol>
-            <CFormInput
-              id={field.name}
+            <Controller
+              control={control}
               name={field.name}
-              label={field.label}
-              type={field.type === 'date' ? 'text' : field.type}
-              placeholder={field.placeholder}
-              onChange={handleChange}
-              disabled={field.isDisabled}
-              readOnly={isReadMode}
-              value={fieldValue || ''}
-              min={field.type === 'number' ? 0 : ''}
+              rules={field.rules}
+              defaultValue={field.defaultValue}
+              render={({ field: { onChange, onBlur, name } }) => {
+                const fieldValue = getFieldValue(field, formData);
+
+                return (
+                  <CFormInput
+                    id={field.id}
+                    name={name}
+                    label={field.label}
+                    type={field.type === 'date' ? 'text' : field.type}
+                    min={field.type === 'number' ? 0 : ''}
+                    placeholder={field.placeholder}
+                    value={fieldValue}
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    readOnly={isReadMode}
+                    disabled={field.isDisabled}
+                    invalid={errors[field.name]}
+                    feedbackInvalid={errors[field.name] && errors[field.name].message}
+                  />
+                );
+              }}
             />
           </CCol>
         </CRow>
