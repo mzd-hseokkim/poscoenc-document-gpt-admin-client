9d98c9908c2485d49a2613465ab81037930ca403
mz-imhj <imhj@mz.co.kr>
2024-02-27 16:16:10 +0900

    Add ProtectRoutes Fix 404 Page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './scss/style.scss';
 import SetupInterceptors from './api/SetupInterceptors';
 import { NavigationProvider } from './context/NavigationContext';
 import { ToastProvider } from './context/ToastContext';
+import { ProtectRoutes } from './routes/ProtectRoutes';
 
 const loading = (
   <div className="pt-3 text-center">
@@ -31,11 +32,13 @@ class App extends Component {
             <SetupInterceptors />
             <Suspense fallback={loading}>
               <Routes>
-                <Route exact path="/sign-in" name="Sign-In Page" element={<SignIn />} />
-                <Route exact path="/register" name="Register Page" element={<Register />} />
-                <Route exact path="/404" name="Page 404" element={<Page404 />} />
-                <Route exact path="/500" name="Page 500" element={<Page500 />} />
-                <Route path="*" name="Home" element={<DefaultLayout />} />
+                <Route element={<ProtectRoutes />}>
+                  <Route exact path="/sign-in" name="Sign-In Page" element={<SignIn />} />
+                  <Route exact path="/register" name="Register Page" element={<Register />} />
+                  <Route exact path="/404" name="Page 404" element={<Page404 />} />
+                  <Route exact path="/500" name="Page 500" element={<Page500 />} />
+                  <Route path="*" name="Home" element={<DefaultLayout />} />
+                </Route>
               </Routes>
             </Suspense>
           </ToastProvider>
