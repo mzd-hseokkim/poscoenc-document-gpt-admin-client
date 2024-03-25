5ea4670b2c454af32d0c0c6246ddd7ff2fd3d910
SonYongGyun <omega@mz.co.kr>
2024-03-25 17:07:51 +0900

    Try to add depth on sidebar menu

--- src/components/AppSidebarNav.js
+++ src/components/AppSidebarNav.js
@@ -40,12 +40,17 @@ export const AppSidebarNav = ({ items, refetchMenuList }) => {
     );
   };
 
-  const navItem = (item, index) => {
+  const navItem = (item, index, depth = 0) => {
     const { component, name, badge, icon, isFavorite, favoriteMenuId, id, ...rest } = item;
     const Component = component;
 
     return (
-      <Component {...(rest.to && !rest.items && { component: NavLink })} key={index} {...rest}>
+      <Component
+        {...(rest.to && !rest.items && { component: NavLink })}
+        key={index}
+        {...rest}
+        style={{ ...rest.style, ...getIndentStyle(depth) }}
+      >
         {navLink(name, icon, badge)}
         <CCol className="d-grid justify-content-md-end">
           {isFavorite !== undefined &&
@@ -75,9 +80,13 @@ export const AppSidebarNav = ({ items, refetchMenuList }) => {
     );
   };
 
-  const navGroup = (item, index) => {
+  const navGroup = (item, index, depth = 0) => {
     const { component, name, icon, to, ...rest } = item;
     const Component = component;
+
+    //TODO 여기서 ClassName 을 붙여주는데 최초 사이드바 렌더링 시 즐겨찾기가 열려있게 기본적으로 세팅되어 있어야한다.
+    // 그 후로는 알아서 ... 새로고침하면 즐겨찾기가 열려있게만.
+    // navGroup 의 className 을 고정시켜놔도 적용되는건 없다.
 
     return (
       <Component
@@ -85,9 +94,12 @@ export const AppSidebarNav = ({ items, refetchMenuList }) => {
         key={index}
         toggler={navLink(name, icon)}
         visible={location.pathname.startsWith(to)}
+        style={getIndentStyle(depth)}
         {...rest}
       >
-        {item.items?.map((item, index) => (item.items ? navGroup(item, index) : navItem(item, index)))}
+        {item.items?.map((subItem, subIndex) =>
+          subItem.items ? navGroup(subItem, subIndex, depth + 1) : navItem(subItem, subIndex, depth + 1)
+        )}
       </Component>
     );
   };
@@ -99,6 +111,9 @@ export const AppSidebarNav = ({ items, refetchMenuList }) => {
   );
 };
 
+const getIndentStyle = (depth) => ({
+  paddingLeft: `${depth * 20}px`,
+});
 AppSidebarNav.propTypes = {
   items: PropTypes.arrayOf(PropTypes.any).isRequired,
 };
