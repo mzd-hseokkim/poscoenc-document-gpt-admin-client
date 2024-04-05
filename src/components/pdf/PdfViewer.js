5fcfb71d873436aa6aae65f57fe577983ec8de6a
SonYongGyun <omega@mz.co.kr>
2024-04-05 17:57:23 +0900

    Fix document page flashing issue

--- src/components/pdf/PdfViewer.js
+++ src/components/pdf/PdfViewer.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 
 import { cilArrowCircleLeft, cilArrowCircleRight } from '@coreui/icons';
 import CIcon from '@coreui/icons-react';
@@ -17,8 +17,7 @@ const PdfViewer = ({ file, visible }) => {
   const [scale, setScale] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
   const token = useRecoilValue(jwtTokenState);
-  const pageCanvasRef = useRef(null);
-
+  const [pageHeight, setPageHeight] = useState('100px');
   const options = useMemo(
     () => ({
       httpHeaders: {
@@ -28,9 +27,14 @@ const PdfViewer = ({ file, visible }) => {
     [token]
   );
 
-  const [pageLoadingIconClassName, setPageLoadingIconClassName] = useState('horizontal-doc');
   const onDocumentLoadSuccess = ({ numPages }) => {
     setNumPages(numPages);
+
+    if (scrollToViewer.current && visible) {
+      setTimeout(() => {
+        scrollToViewer.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+      }, 300);
+    }
   };
 
   const goToPreviousPage = () => {
@@ -41,6 +45,16 @@ const PdfViewer = ({ file, visible }) => {
     setCurrentPage(currentPage < numPages ? currentPage + 1 : numPages);
   };
 
+  const scrollToViewer = useRef(null);
+
+  useEffect(() => {
+    if (visible) {
+      const canvasElement = document.querySelector('.react-pdf__Page__canvas');
+      if (canvasElement) {
+        setPageHeight(`${canvasElement.offsetHeight}px`);
+      }
+    }
+  }, [currentPage, visible]);
   if (!visible) return;
 
   return (
@@ -61,7 +75,11 @@ const PdfViewer = ({ file, visible }) => {
               className="pdf-viewer-document"
               file={`${SERVER_ENDPOINT}/admin/document-collection-files/download/${file.id}`}
               onLoadSuccess={onDocumentLoadSuccess}
-              loading={<CSpinner variant={'grow'} className={`${pageLoadingIconClassName}`} />}
+              loading={
+                <div style={{ minHeight: '100px', minWidth: '100px' }} className="pdf-page-loading-cover">
+                  <CSpinner variant={'grow'} color={'primary'} />
+                </div>
+              }
               error={
                 <div className="d-flex justify-content-center bold">{'PDF 파일을 로딩하는 데에 실패하였습니다.'}</div>
               }
@@ -73,20 +91,25 @@ const PdfViewer = ({ file, visible }) => {
                 pageNumber={currentPage}
                 scale={scale}
                 error="PDF 파일을 로딩하는 데에 실패하였습니다."
-                loading={<CSpinner variant={'grow'} className={`pdf-page-loading-cover ${pageLoadingIconClassName}`} />}
+                loading={
+                  <div style={{ minHeight: pageHeight, minWidth: '100px' }} className="pdf-page-loading-cover">
+                    <CSpinner variant={'border'} color={'primary'} />
+                  </div>
+                }
                 renderTextLayer={false}
-                className="d-flex mt-2  justify-content-center align-items-center"
+                className="d-flex mt-2  justify-content-center align-items-center border"
                 renderAnnotationLayer={false}
                 width={656}
-                canvasRef={pageCanvasRef}
               />
             </Document>
-            <CButton className="pagination-btn me-2" onClick={goToPreviousPage}>
-              <CIcon style={{ height: 20, width: 20, marginTop: 4 }} icon={cilArrowCircleLeft} />
-            </CButton>
-            <CButton className="pagination-btn" onClick={goToNextPage}>
-              <CIcon style={{ height: 20, width: 20, marginTop: 4 }} icon={cilArrowCircleRight} />
-            </CButton>
+            <CRow ref={scrollToViewer} className="mt-2 d-flex justify-content-center">
+              <CButton className="pagination-btn me-2" onClick={goToPreviousPage}>
+                <CIcon style={{ height: 20, width: 20, marginTop: 4 }} icon={cilArrowCircleLeft} />
+              </CButton>
+              <CButton className="pagination-btn" onClick={goToNextPage}>
+                <CIcon style={{ height: 20, width: 20, marginTop: 4 }} icon={cilArrowCircleRight} />
+              </CButton>
+            </CRow>
           </CRow>
         </>
       </div>
