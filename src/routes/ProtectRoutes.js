b92a2f50e4fe2efcdfa5fdb219fd5426232cf30c
SonYongGyun <omega@mz.co.kr>
2024-06-24 10:38:10 +0900

    Modify Api.js to handle all 401 errors

--- src/routes/ProtectRoutes.js
+++ src/routes/ProtectRoutes.js
@@ -29,11 +29,7 @@ export const ProtectRoutes = () => {
         } catch (error) {
           const status = error.response?.status;
           setHasError(true);
-          if (status === 401) {
-            navigate('/sign-in', { replace: true });
-            localStorage.removeItem('token');
-            addToast({ message: '세션이 만료되었습니다. 다시 로그인 해주세요. log:ProtectRoutes' }, false);
-          } else if (status === 403) {
+          if (status === 403) {
             navigate('/', { replace: true });
             addToast('접근 권한이 없습니다');
           } else if (status === 404) {
