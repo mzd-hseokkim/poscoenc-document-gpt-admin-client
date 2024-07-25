81636dc0b030d4034df5089ef2371ed9fa88c0eb
SonYongGyun <omega@mz.co.kr>
2024-07-25 16:09:03 +0900

    Add Dismiss all toast button

--- src/context/ToastContext.js
+++ src/context/ToastContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useRef, useState } from 'react';
 
-import { CToast, CToastBody, CToastClose, CToaster } from '@coreui/react-pro';
+import { CButton, CToast, CToastBody, CToastClose, CToaster } from '@coreui/react-pro';
 
 const ToastContext = createContext({});
 
@@ -18,26 +18,44 @@ export const ToastProvider = ({ children }) => {
     setToasts((prevToasts) => prevToasts.filter((toast) => toast.key !== key));
   };
 
+  const clearAllToasts = () => {
+    setToasts([]);
+  };
+
   return (
-    <ToastContext.Provider value={{ addToast }}>
-      {children}
-      <CToaster placement="bottom-end">
-        {toasts.map((toast) => (
-          <CToast
-            autoFocus
-            key={toast.key}
-            autohide={toast.autoHide}
-            visible={true}
-            color={toast.color}
-            className="d-flex"
-            onClose={() => removeToast(toast.key)}
+    <>
+      <ToastContext.Provider value={{ addToast }}>
+        {children}
+        <CToaster placement="bottom-end">
+          {toasts.map((toast, index) => (
+            <CToast
+              autoFocus
+              key={toast.key}
+              autohide={toast.autoHide}
+              visible={true}
+              color={toast.color}
+              className="d-flex"
+              onClose={() => removeToast(toast.key)}
+            >
+              <CToastBody className="text-white">{toast.message + index}</CToastBody>
+              <CToastClose className="me-2 m-auto" white />
+            </CToast>
+          ))}
+        </CToaster>
+        {toasts.length > 0 && (
+          <CButton
+            style={{
+              position: 'fixed',
+              right: 370,
+              bottom: 16,
+            }}
+            onClick={clearAllToasts}
           >
-            <CToastBody className="text-white">{toast.message}</CToastBody>
-            <CToastClose className="me-2 m-auto" white />
-          </CToast>
-        ))}
-      </CToaster>
-    </ToastContext.Provider>
+            {'Dismiss All'}
+          </CButton>
+        )}
+      </ToastContext.Provider>
+    </>
   );
 };
 
